7ebd8d869b947842da22122d1a6df8bca664abb9
AmurKhoyetsyan <javanewss@gmail.com>
2026-01-17 14:34:55 +0400

    replace partials

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const { Builder, By, until } = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
+const config = require('./module/config');
 
 (async function run() {
-    const options = new chrome.Options();
-    options.addArguments('--start-maximized');
-
-    const driver = await new Builder()
+    const browser = new webdriver.Builder()
         .forBrowser('chrome')
-        .setChromeOptions(options)
         .build();
 
-    try {
-        await driver.get('https://example.com');
-
-        await driver.wait(until.elementLocated(By.tagName('h1')), 5000);
-
-        const text = await driver.findElement(By.tagName('h1')).getText();
-        console.log(text);
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await driver.quit();
-    }
+    await browser.get(config.baseUrl);
 })();
